d6356b6c6ccaf16cc340c6d807453a8cbc6c0fe3
pablo-lucas <pablo.lucv@gmail.com>
2022-08-15 19:33:13 -0500

    files refactor

---
+++ src/state/actions/index.ts
@@ -0,0 +1,19 @@
+import { ActionType } from "../action-types";
+
+interface SearchRepositoriesAction {
+    type: ActionType.SEARCH_REPOSITORIES;
+    payload: string;
+}
+
+interface SearchRepositoriesSuccessAction {
+    type: ActionType.SEARCH_REPOSITORIES_SUCCESS;
+    payload: [];
+
+}
+
+interface SearchRepositoriesErrorAction {
+    type: ActionType.SEARCH_REPOSITORIES_ERROR;
+    payload: string;
+}
+
+export type Action = SearchRepositoriesAction | SearchRepositoriesSuccessAction | SearchRepositoriesErrorAction
